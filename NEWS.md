e293384762f20247b2d2f7cf47a31e7f821c6b77
Tom Elliott <tomelliottnz@gmail.com>
2019-07-22 11:28:46 +1200

    Install binary on win (fix #3)

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# iNZightUpdate 0.0.2
+
+- install only binary versions of packages on windows and mac
+
 # iNZightUpdate 0.0.1
 
 Initial release of the package, which does a simple `update.packages()` in the library dir for iNZight on the specified OS.
--- R/update.R
+++ R/update.R
@@ -13,7 +13,7 @@ update <- function(os = c("windows", "macos", "linux")) {
     cat("* iNZightVIT was updated successfully\n")
 }
 
-.update <- function(lib) {
+.update <- function(lib, type = "source") {
     cat(sprintf("* Updating packages in %s\n", lib))
     x <- try(utils::update.packages(
         lib.loc = lib, 
@@ -21,17 +21,20 @@ update <- function(os = c("windows", "macos", "linux")) {
             "https://r.docker.stat.auckland.ac.nz",
             "https://cloud.r-project.org"
         ),
+        type = type,
         ask = FALSE
     ), silent = TRUE)
     !inherits(x, "try-error")
 }
 
 update_windows <- function() {
-    .update(.libPaths()[1])
+    .update(.libPaths()[1], type = "win.binary")
 }
 
 update_macos <- function() {
     .update("/Applications/iNZightVIT/.library")
+        # and either type = "mac.binary.el-capitan")
+        # or type = "mac.binary") ???
 }
 
 update_linux <- function() {
