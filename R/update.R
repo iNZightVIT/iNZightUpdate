bb9ac12c426f013c3566b211d10a67799514aab8
Tom Elliott <tomelliottnz@gmail.com>
2020-07-09 16:50:21 +1200

    Update update.R

--- R/update.R
+++ R/update.R
@@ -7,7 +7,7 @@
 update <- function(os = c("windows", "macos", "linux")) {
     os <- match.arg(os)
 
-    if (package_version("iNZight") < numeric_version("4.0.0")) {
+    if (packageVersion("iNZight") < numeric_version("4.0.0")) {
         msg <- c(
             "The latest version, iNZight 4.0, contains some bigger changes",
             "than usual. It also has some updated dependencies, so please",
